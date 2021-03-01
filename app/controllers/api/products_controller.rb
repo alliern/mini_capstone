6da5fbb0e6bedb2fb6a7b93e6915753317cd7267
Allie Neal <allieneal93@gmail.com>
2021-03-01 14:44:53 -0500

    adds update and destroy paths

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -17,26 +17,32 @@ class Api::ProductsController < ApplicationController
   end
 
   def create
-    {
-    @name: params[:name],
-    @price: params[:price],
-    @image_url: params[:image_url],
-    @description: params[:description],
-  }
+    @product = Product.new
+    @product.name = params[:name]
+    @product.price = params[:price]
+    @product.image_url = params[:image_url]
+    @product.description = params[:description]
+    @product.save
   render "show.json.jb"
   end
   
   def update
     product_id = params[:id]
-    @product = Product.find(id: product_id)
+    @product = Product.find_by(id: product_id)
     
-      @name: params[:name] || @product.name,
-      @price: params[:price] || @product.price,
-      @image_url: params[:image_url] || @product.image_url,
-      @description: params[:description] || @product.description
-      @product.save
-      render "show.json.jb"
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.image_url = params[:image_url] || @product.image_url
+    @product.description = params[:description] || @product.description
+    @product.save
+    render "show.json.jb"
     
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.delete
+    render json: {message: "Your product was successfully deleted!"}
   end
 
 
