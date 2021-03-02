68a4305c30c32724cda526b4615dfbad5472623b
Allie Neal <allieneal93@gmail.com>
2021-03-02 13:46:29 -0500

    adds partial file

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,20 @@
 class Product < ApplicationRecord
+
+  def is_discounted?
+    if price < 15
+      true
+    else
+      false
+    end
+  end
+
+  def tax
+    tax = price * 0.09
+    tax
+  end
+
+  def total
+    total = price + tax
+    total
+  end
 end
