7030e1c5db316a6923ebe3ee957adec00b866947
Allie Neal <allieneal93@gmail.com>
2021-03-10 13:46:53 -0500

    adds index and show

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,19 +1,36 @@
 class Api::OrdersController < ApplicationController
 
   def create
-    if current_user
-    @order = Order.new
-    @order.user_id = params[:user_id]
-    @order.product_id = params[:product_id]
-    @order.quantity = params[:quantity]
-    # @order.subtotal = params[:subtotal]
-    # @order.tax = params[:tax]
-    # @order.total = params[:total]
-    if @order.save
-    render "order.json.jb"
-    else
-      render json: {error: "User must be signed in"}
-    end
-    end
+    product = Product.find_by(id: params[:product_id])
+
+    calculated_subtotal = product.price * params[:quantity].to_i
+    calculated_tax = calculated_subtotal * 0.09
+    calculated_total = calculated_subtotal + calculated_tax
+
+    @order = Order.new(
+      user_id: current_user.id,
+      product_id: params[:product_id],
+      quantity: params[:quantity],
+      subtotal: calculated_subtotal,
+      tax: calculated_tax,
+      total: calculated_total
+    )
+    @order.save
+    render "show.json.jb"
+    # else
+    #   render json: {error: "User must be signed in"}
+    # end
+  
   end
+
+  def index
+    @orders = current_user.order
+    render "index.json.jb"
+  end
+
+  def show
+    order_id = params[:id]
+    @order = current_user.orders.find(order_id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
     post "/sessions" => "sessions#create"
 
     post "/orders" => "orders#create"
+    get "/orders" => "orders#index"
   end
 end
